27f84debe547f4b2fe6852763a319463115e550d
Janet Suryadi <janet@suryadi.id.au>
2023-07-11 20:54:01 +1000

    Starting Landing section by copying over old portfolio landing section

---
+++ src/components/sections/LandingSection/LandingSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FontAwesomeIcon, faWindowMinimize } from '@fortawesome/react-fontawesome'
+
+const LandingSection = () => {
+    return (
+        <>
+            <section className="landing">
+                {/* Add a picture of a rotating computer monitor here */}
+                <img src="https://picsum.photos/id/684/600/400" alt="" />
+                    <div className="landing__text">
+                        <h2 className="landing__command"></h2>
+                        <h3 className="landing__message landing__message--first"></h3>
+                        <h3 className="landing__message landing__message--second"></h3><span><i
+                            className="fas fa-window-minimize landing__title--cursoricon fa-fade"></i></span>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default LandingSection
