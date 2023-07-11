250c3d4a46ea86f7853a1f6cb113fec4ede93ef4
Janet Suryadi <janet@suryadi.id.au>
2023-07-11 20:20:16 +1000

    Starting Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.scss'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <img src={reactLogo} className='logo react'/>
     </>
   )
